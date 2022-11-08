4dc35d85a14368bc32db5fd3526e327115578d9c
Georgiy Nozdrin <georgiynozdrin@gmail.com>
2022-11-08 23:09:47 +0700

    feat(1-ether-wallet): add transfer multiple function

--- 1-ether-wallet/contracts/EtherWallet.sol
+++ 1-ether-wallet/contracts/EtherWallet.sol
@@ -6,9 +6,15 @@ import '@openzeppelin/contracts/utils/Address.sol';
 contract EtherWallet {
     mapping (address => uint256) private wallet;
 
+    struct STransfer {
+        address to;
+        uint256 amount;
+    }
+
     event Deposit(uint256 value);
     event Withdraw(uint256 value);
     event Transfer(uint256 value);
+    event TransferMultiple(uint256 value);
 
     function deposit() payable external {
         wallet[msg.sender] += msg.value;
@@ -25,13 +31,30 @@ contract EtherWallet {
         emit Withdraw(_amount);
     }
 
-    function transfer(address _to, uint256 _amount) external {
-        require(balance() >= _amount, 'Not enought balance');
+    function transfer(STransfer memory _transfer) external {
+        require(balance() >= _transfer.amount, 'Not enought balance');
 
-        wallet[msg.sender] -= _amount;
-        wallet[_to] += _amount;
+        wallet[msg.sender] -= _transfer.amount;
+        wallet[_transfer.to] += _transfer.amount;
 
-        emit Transfer(_amount);
+        emit Transfer(_transfer.amount);
+    }
+
+    function transferMultiple(STransfer[] memory _transferItems) external {
+        uint256 totalAmount = 0;
+
+        for (uint256 i = 0; i < _transferItems.length; i++) {
+            totalAmount += _transferItems[i].amount;
+        }
+
+        require(balance() >= totalAmount, 'Not enought balance');
+
+        for (uint256 i = 0; i < _transferItems.length; i++) {
+            wallet[msg.sender] -= _transferItems[i].amount;
+            wallet[_transferItems[i].to] += _transferItems[i].amount;
+        }
+
+        emit TransferMultiple(totalAmount);
     }
 
     function balance() view public returns(uint256) {
--- 1-ether-wallet/test/EtherWallet.js
+++ 1-ether-wallet/test/EtherWallet.js
@@ -1,8 +1,11 @@
 const EtherWallet = artifacts.require('EtherWallet')
 const { shouldThrowError } = require('../helpers/ShouldThrowError')
 
-const ONE_ETH_IN_WEI = web3.utils.toWei(`1`, `ether`)
-const TWO_ETH_IN_WEI = web3.utils.toWei(`2`, `ether`)
+const ONE_GWEI = web3.utils.toWei(`1`, `gwei`)
+const TWO_GWEI = web3.utils.toWei(`2`, `gwei`)
+
+const ONE_ETHER = web3.utils.toWei(`1`, `ether`)
+const TWO_ETHER = web3.utils.toWei(`2`, `ether`)
 
 contract(`EtherWallet`, (accounts) => {
     let contract
@@ -21,15 +24,15 @@ contract(`EtherWallet`, (accounts) => {
 
     context('Deposit', async () => {
         it(`Should deposit`, async () => {
-            const { receipt } = await contract.deposit({ value: ONE_ETH_IN_WEI, from: accounts[0] })
+            const { receipt } = await contract.deposit({ value: ONE_ETHER, from: accounts[0] })
             const balanceContractInWeiBn = await contract.balance({ from: accounts[0] })
 
             assert.equal(receipt.status, true)
-            assert.equal(balanceContractInWeiBn.toString(), ONE_ETH_IN_WEI)
+            assert.equal(balanceContractInWeiBn.toString(), ONE_ETHER)
         })
 
         it(`Should receive [Deposit] event`, async () => {
-            const depositValue = ONE_ETH_IN_WEI
+            const depositValue = ONE_ETHER
 
             const { receipt } = await contract.deposit({ value: depositValue, from: accounts[0] })
 
@@ -44,9 +47,9 @@ contract(`EtherWallet`, (accounts) => {
 
     context('Withdraw', async () => {
         it(`Should withdraw`, async () => {
-            await contract.deposit({ value: ONE_ETH_IN_WEI, from: accounts[0] })
+            await contract.deposit({ value: ONE_ETHER, from: accounts[0] })
 
-            const { receipt } = await contract.withdraw(ONE_ETH_IN_WEI, { from: accounts[0] })
+            const { receipt } = await contract.withdraw(ONE_ETHER, { from: accounts[0] })
             const balanceContractInWeiBn = await contract.balance({ from: accounts[0] })
 
             assert.equal(receipt.status, true)
@@ -54,18 +57,18 @@ contract(`EtherWallet`, (accounts) => {
         })
 
         it(`Should withdraw partial`, async () => {
-            await contract.deposit({ value: TWO_ETH_IN_WEI, from: accounts[0] })
+            await contract.deposit({ value: TWO_ETHER, from: accounts[0] })
 
-            const { receipt } = await contract.withdraw(ONE_ETH_IN_WEI, { from: accounts[0] })
+            const { receipt } = await contract.withdraw(ONE_ETHER, { from: accounts[0] })
             const balanceContractInWeiBn = await contract.balance({ from: accounts[0] })
 
             assert.equal(receipt.status, true)
-            assert.equal(balanceContractInWeiBn.toString(), ONE_ETH_IN_WEI)
+            assert.equal(balanceContractInWeiBn.toString(), ONE_ETHER)
         })
 
         it(`Should receive [Withdraw] event`, async () => {
-            const depositValue = TWO_ETH_IN_WEI
-            const withdrawValue = ONE_ETH_IN_WEI
+            const depositValue = TWO_ETHER
+            const withdrawValue = ONE_ETHER
 
             await contract.deposit({ value: depositValue, from: accounts[0] })
 
@@ -80,7 +83,7 @@ contract(`EtherWallet`, (accounts) => {
         })
 
         it('Should throw error when balance is not enought', async () => {
-            await shouldThrowError(contract.withdraw(ONE_ETH_IN_WEI, { from: accounts[0] }), {
+            await shouldThrowError(contract.withdraw(ONE_ETHER, { from: accounts[0] }), {
                 onError: ({ reason }) => assert.equal(reason, 'Not enought balance'),
             })
         })
@@ -88,45 +91,84 @@ contract(`EtherWallet`, (accounts) => {
 
     context('Transfer', async () => {
         it(`Should transfer`, async () => {
-            await contract.deposit({ value: ONE_ETH_IN_WEI, from: accounts[0] })
+            await contract.deposit({ value: ONE_ETHER, from: accounts[0] })
 
-            const { receipt } = await contract.transfer(accounts[1], ONE_ETH_IN_WEI, { from: accounts[0] })
+            const { receipt } = await contract.transfer({ to: accounts[1], amount: ONE_GWEI }, { from: accounts[0] })
             const balanceContractInWeiBnAccount0 = await contract.balance({ from: accounts[0] })
             const balanceContractInWeiBnAccount1 = await contract.balance({ from: accounts[1] })
 
             assert.equal(receipt.status, true)
-            assert.equal(balanceContractInWeiBnAccount0.toString(), '0')
-            assert.equal(balanceContractInWeiBnAccount1.toString(), ONE_ETH_IN_WEI)
-        })
-
-        it(`Should transfer partial`, async () => {
-            await contract.deposit({ value: TWO_ETH_IN_WEI, from: accounts[0] })
-
-            const { receipt } = await contract.transfer(accounts[1], ONE_ETH_IN_WEI, { from: accounts[0] })
-            const balanceContractInWeiBnAccount0 = await contract.balance({ from: accounts[0] })
-            const balanceContractInWeiBnAccount1 = await contract.balance({ from: accounts[1] })
-
-            assert.equal(receipt.status, true)
-            assert.equal(balanceContractInWeiBnAccount0.toString(), ONE_ETH_IN_WEI)
-            assert.equal(balanceContractInWeiBnAccount1.toString(), ONE_ETH_IN_WEI)
+            assert.equal(balanceContractInWeiBnAccount0.toString(), web3.utils.toBN(ONE_ETHER).sub(web3.utils.toBN(ONE_GWEI)).toString())
+            assert.equal(balanceContractInWeiBnAccount1.toString(), ONE_GWEI)
         })
 
         it(`Should receive [Transfer] event`, async () => {
-            await contract.deposit({ value: TWO_ETH_IN_WEI, from: accounts[0] })
+            await contract.deposit({ value: TWO_ETHER, from: accounts[0] })
 
-            const { receipt } = await contract.transfer(accounts[1], ONE_ETH_IN_WEI, { from: accounts[0] })
+            const { receipt } = await contract.transfer({ to: accounts[1], amount: ONE_ETHER }, { from: accounts[0] })
 
             const event = receipt.logs[0]
             const eventName = event?.event
             const eventValue = event?.args.value.toString()
 
             assert.equal(eventName, 'Transfer')
-            assert.equal(eventValue, ONE_ETH_IN_WEI)
+            assert.equal(eventValue, ONE_ETHER)
         })
 
         it('Should throw error when balance is not enought', async () => {
-            await shouldThrowError(contract.transfer(accounts[1], ONE_ETH_IN_WEI, { from: accounts[0] }), {
-                onError: ({ reason }) => assert.equal(reason, 'Not enought balance'),
+            await contract.deposit({ value: ONE_GWEI, from: accounts[0] })
+
+            await shouldThrowError(contract.transfer({ to: accounts[1], amount: TWO_GWEI }, { from: accounts[0] }), {
+                onError: (error) => assert.equal(error.reason, 'Not enought balance', error.message),
+            })
+        })
+    })
+
+    context('TransferMultiple', async () => {
+        it(`Should transfer`, async () => {
+            await contract.deposit({ value: ONE_ETHER, from: accounts[0] })
+
+            const transferItems = [
+                { to: accounts[1], amount: ONE_GWEI },
+                { to: accounts[2], amount: TWO_GWEI },
+            ]
+
+            const { receipt } = await contract.transferMultiple(transferItems, { from: accounts[0] })
+            const balanceContractInWeiBnAccount0 = await contract.balance({ from: accounts[0] })
+            const balanceContractInWeiBnAccount1 = await contract.balance({ from: accounts[1] })
+            const balanceContractInWeiBnAccount2 = await contract.balance({ from: accounts[2] })
+
+            assert.equal(receipt.status, true)
+            assert.equal(balanceContractInWeiBnAccount0.toString(), web3.utils.toBN(ONE_ETHER).sub(web3.utils.toBN(ONE_GWEI)).sub(web3.utils.toBN(TWO_GWEI)).toString())
+            assert.equal(balanceContractInWeiBnAccount1.toString(), ONE_GWEI)
+            assert.equal(balanceContractInWeiBnAccount2.toString(), TWO_GWEI)
+        })
+
+        it(`Should receive [TransferMultiple] event`, async () => {
+            await contract.deposit({ value: ONE_ETHER, from: accounts[0] })
+
+            const transferItems = [
+                { to: accounts[1], amount: ONE_GWEI },
+                { to: accounts[2], amount: TWO_GWEI },
+            ]
+
+            const { receipt } = await contract.transferMultiple(transferItems, { from: accounts[0] })
+
+            const event = receipt.logs[0]
+            const eventName = event?.event
+            const eventValue = event?.args.value.toString()
+
+            assert.equal(eventName, 'TransferMultiple')
+            assert.equal(eventValue, web3.utils.toBN(ONE_GWEI).add(web3.utils.toBN(TWO_GWEI)).toString())
+        })
+
+        it('Should throw error when balance is not enought', async () => {
+            await contract.deposit({ value: ONE_GWEI, from: accounts[0] })
+
+            const transferItems = [{ to: accounts[1], amount: TWO_GWEI }]
+
+            await shouldThrowError(contract.transferMultiple(transferItems, { from: accounts[0] }), {
+                onError: (error) => assert.equal(error.reason, 'Not enought balance', error.message),
             })
         })
     })
